085841cbecf97dd215a3f5c462b8520801ecdf20
NioDeBacker <nio.debacker@student.hogent.be>
2023-04-09 23:23:35 +0200

    working flip card

--- src/components/flashcard.js
+++ src/components/flashcard.js
@@ -1,26 +1,13 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 import { wait } from "@testing-library/user-event/dist/utils";
 import React from "react";
+import "./flashcard.css";
 
 export default function FlashCard({id, question, code, image, math, answer}) {
     const [flip, setFlip] = React.useState(false);
     return (
-        <Card className={`${flip ? "flip" : ""}`}
-        sx={{width: "160px",
-        transform: "rotateY(var(--rotate-y, 0))",
-        transformStyle: "preserve-3d",
-        transition: "1500ms",
-        cursor: "pointer",
-        transform: "perspective(5000px) rotateY(var(--rotate-y, 0)) translateY(var(--translate-y, 0))",
-        "&:active": {
-            translate: "-2px",
-            boxShadow: "0 0 5px 2px rgba(0, 0, 0, .5)"
-        },
-        "&.flip": {
-            rotate: "y 180deg",
-        }
-        }}>
-                        <CardActionArea onClick={() => {
+        <Card sx={{maxWidth: "250px"}}>
+        <CardActionArea sx={{display: "flex"}} className={`card ${flip ? "flip" : ""}`} onClick={() => {
                             setFlip(!flip);}}>
 
             {image ?             <CardMedia
@@ -28,19 +15,15 @@ export default function FlashCard({id, question, code, image, math, answer}) {
                 image={image}
                 title="image"
             /> : ""}
-            { flip ?             <CardContent sx={{ backfaceVisibility: "hidden",
-  transform: "rotateX(0deg)"}} className="flashcard-front">
+            <CardContent className="front">
                 <Typography sx={{ fontSize: 14}} color="text.secondary" gutterBottom>Quiz nr: {id}</Typography>
                 <Typography variant="body2">{question}</Typography>
                 {code ? <Typography variant="body2">{code}</Typography> : ""}
                 {math ? <div>{math}</div> : ""}
-            </CardContent> :             <CardContent sx={{
-                
-                  transform: "rotateY(180deg)"
-            }} className="flashcard-back">
+            </CardContent> <CardContent s className="back">
                 <Typography variant="body2">{answer}</Typography>
             </CardContent>
-            }
+        
 
 
 
